14938bf104e77436755afb84de0d94a43eed7024
Jennifer Reyna <jennifer.m.reyna@outlook.com>
2021-04-29 23:09:31 -0500

    labels

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -25,7 +25,9 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data from CSV
-d3.csv("data.csv").then(function(statesData){
+d3.csv("data.csv").then(function(statesData, err){
+  if (err) throw err;
+  console.log(statesData);)
 
     // Step 1: Parse Data
     // ==============================
@@ -58,7 +60,7 @@ d3.csv("data.csv").then(function(statesData){
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain([1, d3.max(statesData, d => d.healthcare)])
+      .domain([3, d3.max(statesData, d => d.healthcare)])
       .range([height, 0]);
 
     // Step 3: Create axis functions
@@ -83,32 +85,32 @@ d3.csv("data.csv").then(function(statesData){
     .append("circle")
     .attr("cx", d => xLinearScale(d.poverty))
     .attr("cy", d => yLinearScale(d.healthcare))
-    .attr("r", "20")
-    .attr("fill", "pink")
+    .attr("r", "15")
+    .attr("fill", "lightblue")
     .attr("opacity", ".5");
 
-    // Step 6: Initialize tool tip
-    // ==============================
-    // var toolTip = d3.tip()
-    //   .attr("class", "tooltip")
-    //   .offset([80, -60])
-    //   .html(function(d) {
-    //     return (`${d.rockband}<br>States: ${d.hair_length}<br>Hits: ${d.num_hits}`);
-    //   });
+    // // Step 6: Initialize tool tip
+    // // ==============================
+      var toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function(d) {
+          return (`${statesData}<br>Healthcare: ${d.healthcare}<br>Poverty: ${d.poverty}`);
+        });
 
     // // Step 7: Create tooltip in the chart
     // // ==============================
-    // chartGroup.call(toolTip);
+     chartGroup.call(toolTip);
 
-    // // Step 8: Create event listeners to display and hide the tooltip
-    // // ==============================
-    // circlesGroup.on("click", function(data) {
-    //   toolTip.show(data, this);
-    // })
-    //   // onmouseout event
-    //   .on("mouseout", function(data, index) {
-    //     toolTip.hide(data);
-    //   });
+     // Step 8: Create event listeners to display and hide the tooltip
+     // ==============================
+     circlesGroup.on("click", function(data) {
+       toolTip.show(data, this);
+     })
+       // onmouseout event
+       .on("mouseout", function(data, index) {
+         toolTip.hide(data);
+       });
 
   // Add text into the circle
   chartGroup.selectAll("null")
@@ -119,22 +121,22 @@ d3.csv("data.csv").then(function(statesData){
     .attr("x", d => xLinearScale(d.poverty))
     .attr("y", d => yLinearScale(d.healthcare))
     .attr("text-anchor", "middle")
-    .attr("font-size", "12px")
-    .attr("fill", "white");
+    .attr("font-size", "10px")
+    .attr("fill", "black");
     
   // Create axes labels
     chartGroup.append("text")
       .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left + 40)
+      .attr("y", 0 - margin.left + 50)
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
       .attr("class", "axisText")
-      .text("Healthcare (%)");
+      .text("Lacks Healthcare (%)");
 
     chartGroup.append("text")
-      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 24})`)
       .attr("class", "axisText")
-      .text("Hair Metal Band Hair Length (inches)");
+      .text("In Poverty (%)");
 
 
   }).catch(function(error) {
